393e29dd5a88909a8e2ae55fb7152f48cc0d4865
zipated <zhuo392592689@live.com>
2021-04-05 15:19:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,13 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/openwrt-dev/po2lmo.git
+pushd po2lmo
+make && sudo make install
+popd
+git clone https://github.com/kenzok8/openwrt-packages package/kenzok8
+git clone https://github.com/kenzok8/small.git package/small
+git clone https://github.com/BoringCat/minieap-openwrt.git package/minieap
+git clone https://github.com/BoringCat/luci-app-minieap.git package/luci-app-minieap
+git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
+git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
