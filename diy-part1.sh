0b926eead2101d875b971fde11d5ae6b094846cb
zipafuckgithub <86631350+zipafuckgithub@users.noreply.github.com>
2021-09-28 10:55:40 +0800

    liuran001源测试

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,21 +16,17 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/openwrt-dev/po2lmo.git
-pushd po2lmo
-make && sudo make install
-popd
 
-git clone https://github.com/kenzok8/openwrt-packages package/kenzok8
-git clone https://github.com/kenzok8/small.git package/small
-git clone https://github.com/BoringCat/minieap-openwrt.git package/minieap
-git clone https://github.com/BoringCat/luci-app-minieap.git package/luci-app-minieap
-git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
-git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
+#git clone https://github.com/openwrt-dev/po2lmo.git
+#pushd po2lmo
+#make && sudo make install
+#popd
 
-#echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-#echo 'src-git minieap https://github.com/BoringCat/minieap-openwrt' >>feeds.conf.default
-#echo 'src-git luci-app-minieap https://github.com/BoringCat/luci-app-minieap' >>feeds.conf.default
-#echo 'src-git luci-app-mentohust https://github.com/BoringCat/luci-app-mentohust' >>feeds.conf.default
-#echo 'src-git mentohust https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk' >>feeds.conf.default
+#git clone https://github.com/kenzok8/openwrt-packages package/kenzok8
+#git clone https://github.com/kenzok8/small.git package/small
+#it clone https://github.com/BoringCat/minieap-openwrt.git package/minieap
+#git clone https://github.com/BoringCat/luci-app-minieap.git package/luci-app-minieap
+#git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
+#git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
+
+echo ' src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
